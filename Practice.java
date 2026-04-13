78bb89bb59dd04e06f5f7244b092471607c8e72d
Rousan21 <215422494+Rousan21@users.noreply.github.com>
2026-04-12 23:57:37 -0400

    Lab 6 Completed

--- Practice.java
+++ Practice.java
@@ -3,10 +3,39 @@ import java.util.Scanner;
 public class Practice{
 public static void main(String[] args){
 
-int age = 2;
+Scanner scanner = new Scanner(System.in);
 
-if(age >= 18){
-    System.out.println("You are technically an Adult!");
+//Voting Elgibility
+
+int num;
+String name;
+
+System.out.print("Enter your name guy: ");
+name = scanner.nextLine();
+
+if(name.isEmpty()){
+
+    System.out.print("You didnt enter a name guy...");
+}
+
+else{
+    System.out.print("Hello " + name + "!");
+}
+
+System.out.print("Enter your age scrub: ");
+num = scanner.nextInt();
+
+
+if(num > 18){
+    System.out.println("Not eliglble to vote little bro");
+}
+
+else if(num < 0){
+    System.out.println("You weren't even born yet!");
+}
+
+else{
+    System.out.println("You are old enough to vote!");
 }
 
 }
---
+++ tempCodeRunnerFile.java
@@ -0,0 +1,14 @@
+
+if(num < 0){
+System.out.println("Negative");
+}
+
+else if(num == 0){
+    System.out.println("Zero");
+}
+
+else{
+    System.out.println("Positive");
+}
+
+scanner.close();
